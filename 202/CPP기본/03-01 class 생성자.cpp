0403582fa82a690b992f628bb5c1b9754a94eab1
NK <zzum0701@naver.com>
2022-04-20 09:17:47 +0900

    생성자 호출

--- 202/CPP기본/03-01 class 생성자.cpp
+++ 202/CPP기본/03-01 class 생성자.cpp
@@ -9,7 +9,18 @@ private:
 	int nHakbun;
 	string sName;
 
+public:
 	// 생성자 : 객체가 생성될 때, 호출되는 함수
-	// 디폴트 생성자를 컴파일러가 만들어준다
-	// Student(){}
+	Student(void) {
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+	}
 };
+
+int main(void) {
+	Student stu1;
+	Student stu2 = Student();
+
+	return 0;
+}
