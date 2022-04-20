ea164812c49ef8428acc122ac6ac63a13636a1fa
NK <zzum0701@naver.com>
2022-04-20 09:11:31 +0900

    객체 기본생성(default 생성자)

---
+++ 202/CPP기본/03-01 class 생성자.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+private:
+	int nHakbun;
+	string sName;
+
+	// 생성자 : 객체가 생성될 때, 호출되는 함수
+	// 디폴트 생성자를 컴파일러가 만들어준다
+	// Student(){}
+};
