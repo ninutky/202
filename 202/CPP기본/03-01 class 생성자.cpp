0e805ed05700b5ffc3230cf5d3d7d97935adc99a
NK <zzum0701@naver.com>
2022-04-20 09:43:27 +0900

    멤버함수 추가

--- 202/CPP기본/03-01 class 생성자.cpp
+++ 202/CPP기본/03-01 class 생성자.cpp
@@ -16,11 +16,17 @@ public:
 		sName = "이사랑";
 		cout << "학번이 등록되었습니다." << endl;
 	}
+	void show(void) {
+		cout << "학번은 " << nHakbun << "입니다." << endl;
+		cout << "이름은 " << sName << "입니다." << endl << endl;;
+	}
 };
 
 int main(void) {
 	Student stu1;
 	Student stu2 = Student();
+	stu1.show();
+	stu2.show();
 
 	return 0;
 }
