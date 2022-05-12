2b7abda348a791d8e389618ceea3b4a945da2fa8
NK <zzum0701@naver.com>
2022-05-12 12:16:18 +0900

    소멸자 추가

--- 202/CPP기본/03-01 class 생성자.cpp
+++ 202/CPP기본/03-01 class 생성자.cpp
@@ -12,6 +12,9 @@ private:
 public:
 	// 생성자 : 객체가 생성될 때, 호출되는 함수
 	Student(int Hakbun, const char* Name);
+	// 소멸자 : 객체가 메모리에서 해제될 때, 호출되는 함수
+	~Student();
+
 	void show(void);
 };
 
@@ -31,6 +34,12 @@ Student::Student(int Hakbun, const char* Name)
 	sName = new char[len];		// 그 크기만큼 배열로 동적할당
 	strcpy(sName, Name);		// 매개변수에 있는 문자열 복사
 };
+
+Student::~Student()
+{
+	delete []sName;
+	cout << "소멸자 호출" << endl;
+}
 
 void Student::show(void) {
 	cout << "학번은 " << nHakbun << "입니다." << endl;
