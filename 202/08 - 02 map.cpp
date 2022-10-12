e812a7de4a7effdfda234b922f651c9f53751052
NK <zzum0701@naver.com>
2022-10-12 09:22:38 +0900

    map

---
+++ 202/08 - 02 map.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+// 김신: 공유
+// 지은탁: 김고은
+// 저승사자: 이동욱
+// 써니: 유인나
+
+// arr["김신"] = "공유"
+// arr["저승사자"] = "이동욱"
+
+//key는 실제 값을 가져오기 위한 무언가
+
+int main(void) {
+	// map<key자료형, value자료형>
+	map<string, string> m;
+	m.insert(pair<string, string>("김신", "공유"));
+	m.insert(pair<string, string>("지은탁", "김고은"));
+	m.insert(pair<string, string>("저승사자", "이동욱"));
+	m.insert(pair<string, string>("써니", "유인나"));
+
+	map<string, string>::iterator iter;
+
+	// 벡터의 원소들을 출력
+	// 삽입할 때마다 key는 자동정렬됨
+	for (iter = m.begin(); iter != m.end(); iter++)
+	{
+		cout << iter->first << "역 " << iter->second << "분" << endl;
+	}
+	cout << endl;
+
+	map<string, string>::iterator actor = m.find("김신");
+
+	cout << "주인공은 " << actor->second << endl;
+
+	return 0;
+}
