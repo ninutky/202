c48f32e5db93266381dcdb7917822bc65bb9cda4
NK <zzum0701@naver.com>
2022-10-12 09:44:56 +0900

    map 간편하게

--- 202/08 - 02 map.cpp
+++ 202/08 - 02 map.cpp
@@ -17,14 +17,14 @@ using namespace std;
 int main(void) {
 	// map<key자료형, value자료형>
 	map<string, string> m;
-	m.insert(pair<string, string>("김신", "공유"));
-	m.insert(pair<string, string>("지은탁", "김고은"));
-	m.insert(pair<string, string>("저승사자", "이동욱"));
-	m.insert(pair<string, string>("써니", "유인나"));
+	m["김신"] = "공유";
+	m["지은탁"] = "김고은";
+	m["저승사자"] = "이동욱";
+	m["써니"] = "유인나";
 
 	map<string, string>::iterator iter;
 
-	// 벡터의 원소들을 출력
+	// 맵의 원소들을 출력
 	// 삽입할 때마다 key는 자동정렬됨
 	for (iter = m.begin(); iter != m.end(); iter++)
 	{
