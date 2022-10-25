533be31bb602b04624386692968e765434a127ec
NK <zzum0701@naver.com>
2022-10-26 08:50:03 +0900

    범위기반 for문으로 간단하게

--- 202/09 - 02 범위기반 for.cpp
+++ 202/09 - 02 범위기반 for.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int main(void) {
 	double arr[4] = { 1.1, 2.2, 3.3, 4.4 };
-
-	for (int i = 0; i < 4; i++) {
-		cout << arr[i] << "	";
+	for (double x : arr) {
+		cout << x << "	";
 	}
 
 	return 0;
