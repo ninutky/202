1fb71c1d434a28e791eb915f957fac53b6559357
NK <zzum0701@naver.com>
2022-10-26 08:47:06 +0900

    전통적인 for

---
+++ 202/09 - 02 범위기반 for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	double arr[4] = { 1.1, 2.2, 3.3, 4.4 };
+
+	for (int i = 0; i < 4; i++) {
+		cout << arr[i] << "	";
+	}
+
+	return 0;
+}
