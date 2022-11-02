17731cd6d201441242e26cfc51f3cc66c78fa14c
NK <zzum0701@naver.com>
2022-11-02 10:22:33 +0900

    static 멤버

---
+++ 202/09 - 03 static 멤버.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Champ {
+public:
+    Champ(int age, const string& name)
+        : mAge(age), mName(name)
+    {
+        mCount++;
+    }
+    static int getCount(void) {
+        // 일반 멤버변수는 static 멤버함수에 적용할 수 없다
+        // mAge = 10;
+        return mCount;
+    }
+
+private:
+    int mAge;
+    string mName;
+    static int mCount;
+};
+
+int Champ::mCount = 0;
+
+int main(void)
+{
+    // 객체 없이도 static 멤버함수 호출가능
+    cout << Champ::getCount() << endl;
+
+    Champ* teemo = new Champ(15, "티모");
+    Champ* masteryi = new Champ(20, "마이");
+    Champ* veign = new Champ(24, "베인");
+
+    // mCount는 객체가 공유되어 총 3번 증가
+    cout << Champ::getCount() << endl;
+    cout << teemo->getCount() << endl;
+
+    return 0;
+}
